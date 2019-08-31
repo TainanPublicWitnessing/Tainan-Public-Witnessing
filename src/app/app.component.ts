959f6ca2a5f12908820eb49db9b1c254e6fcc0c6
petertsai0730 <petertsai0730@gmail.com>
2019-08-31 11:34:11 +0800

    bug fixed

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,7 +5,6 @@ import { SplashScreen } from "@ionic-native/splash-screen/ngx";
 import { StatusBar } from "@ionic-native/status-bar/ngx";
 
 import {NavService} from "./service/nav.service";
-import {NavComponent} from "./nav/nav.component";
 
 @Component({
   selector: "app-root",
